9100631d00ae8a2c0f673dcee8596aed3f898503
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-06-07 21:57:47 +1000

    Initial Commit

---
+++ QuarterFlip/QuarterFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuarterFlipApp.swift
+//  QuarterFlip
+//
+//  Created by Wasif Kamruddin on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct QuarterFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
