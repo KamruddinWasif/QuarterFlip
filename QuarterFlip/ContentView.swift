4f15c3b278b0e4acfde19422d9526ea45ae7b90a
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-06-07 23:06:10 +1000

    working coin annimation ready for testflight

--- QuarterFlip/ContentView.swift
+++ QuarterFlip/ContentView.swift
@@ -1,44 +1,67 @@
 // ContentView.swift
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
-    @State private var isFlipping = false
-    @State private var coinSide = "heads"
+    @State private var flipDegrees: Double = 0
+    @State private var isSpinning = false
+    @State private var showingHead = true
+    
+    private let lightHaptic = UIImpactFeedbackGenerator(style: .light)
+    private let heavyHaptic = UIImpactFeedbackGenerator(style: .heavy)
     
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             VStack {
+                Text("QUARTER FLIP")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Spacer()
                 Text("Swipe to flip the coin")
                     .font(.headline)
                     .foregroundColor(.white)
                 Spacer()
-                Image(coinSide)
-                    .resizable()
-                    .scaledToFit()
-                    .rotation3DEffect(
-                        .degrees(isFlipping ? 180 : 0),
-                        axis: (x: 0, y: 1, z: 0)
-                    )
-                    .animation(.interpolatingSpring(mass: 1, stiffness: 100, damping: 10, initialVelocity: 0))
-                    .onAppear() {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            withAnimation {
-                                self.isFlipping.toggle()
-                                self.coinSide = self.coinSide == "heads" ? "tails" : "heads"
-                            }
-                        }
-                    }
+                CoinView(showingHead: $showingHead)
+                    .frame(width: 100, height: 100)
+                    .rotation3DEffect(.degrees(flipDegrees), axis: (x: flipDegrees / 90, y: flipDegrees, z: 0))
+                    .animation(.linear(duration: isSpinning ? 0.1 : 1))
                 Spacer()
             }
         }
         .gesture(DragGesture(minimumDistance: 50)
                     .onEnded({ _ in
                         withAnimation {
-                            self.isFlipping.toggle()
-                            self.coinSide = self.coinSide == "heads" ? "tails" : "heads"
+                            self.isSpinning = true
+                            self.lightHaptic.impactOccurred()
+                            let spinTime = Double.random(in: 1...2)
+                            Timer.scheduledTimer(withTimeInterval: spinTime, repeats: false) { _ in
+                                self.isSpinning = false
+                                self.showingHead = Bool.random()
+                                self.heavyHaptic.impactOccurred()
+                            }
+                            
+                            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                                if !self.isSpinning {
+                                    timer.invalidate()
+                                }
+                                self.flipDegrees += 180
+                            }
                         }
                     }))
+    }
+}
+
+struct CoinView: View {
+    @Binding var showingHead: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.gray)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .overlay(Text(showingHead ? "H" : "T").foregroundColor(.white).font(.title))
+        }
     }
 }
 
